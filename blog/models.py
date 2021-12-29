d161e8636864f9cd678a47d6f5fbc43087de901a
mark-akbarov <mark-akbarov@gmail.com>
2021-12-29 15:37:12 +0500

    Rename post-detail to post_detail

--- blog/models.py
+++ blog/models.py
@@ -14,4 +14,4 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('post-detail', args=[str(self.id)])
+        return reverse('post_detail', args=[str(self.id)])
--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from .views import (
     BlogCreateView, BlogUpdateView, BlogDeleteView
 )
 
+
 urlpatterns = [
     path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
     path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_update'),
