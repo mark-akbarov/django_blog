d8b72f2b6ca25e57fffe26a68278e1ea0882c6a7
mark-akbarov <mark-akbarov@gmail.com>
2021-12-29 14:43:16 +0500

    Add Form, Update and Delete functionality

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import BlogDetailView, BlogListView 
+from .views import BlogDetailView, BlogListView , BlogCreateView, BlogUpdateView, BlogDeleteView
 
 
 urlpatterns = [
+    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post-edit'),
+    path('post/new/', BlogCreateView.as_view(), name='post-new'),
     path('', BlogListView.as_view(), name='home'),
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post-detail')
 ]
